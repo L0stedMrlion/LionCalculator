9c7b62a198d37f182c638ecb2d0345e990ff1358
Mrlion <mrlion4202@gmail.com>
2024-03-06 16:24:50 +0000

    some changes

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,37 +1,21 @@
-#include <iostream>
+#include "calculator.h"
 
-using namespace std;
+double Calculator::add(double num1, double num2) {
+    return num1 + num2;
+}
 
-int main() {
-    char op;
-    double num1, num2;
+double Calculator::subtract(double num1, double num2) {
+    return num1 - num2;
+}
 
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> op;
+double Calculator::multiply(double num1, double num2) {
+    return num1 * num2;
+}
 
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-
-    switch(op) {
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
-            break;
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
-            break;
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0)
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
-            else
-                cout << "Error! Division by zero!";
-            break;
-        default:
-            cout << "Error! Invalid operator";
-            break;
+double Calculator::divide(double num1, double num2) {
+    if(num2 != 0)
+        return num1 / num2;
+    else {
+        return 0;
     }
-
-    return 0;
 }
